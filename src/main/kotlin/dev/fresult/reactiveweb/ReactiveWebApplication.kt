5d7c3a620699470dcb70ec55de03e19b97fa2ecf
fResult Sila <Styxmaz@gmail.com>
2023-12-03 15:45:39 +0700

    First Functional Endpoints

--- src/main/kotlin/dev/fresult/reactiveweb/ReactiveWebApplication.kt
+++ src/main/kotlin/dev/fresult/reactiveweb/ReactiveWebApplication.kt
@@ -2,13 +2,17 @@ package dev.fresult.reactiveweb
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.function.server.RouterFunction
+import org.springframework.web.reactive.function.server.RouterFunctions.route
+import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.server.body
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 import java.time.Duration
-import java.util.stream.Collectors
+import kotlin.time.Duration.Companion.seconds
+import kotlin.time.toJavaDuration
 
 @SpringBootApplication
 class ReactiveWebApplication
@@ -17,26 +21,23 @@ fun main(args: Array<String>) {
   runApplication<ReactiveWebApplication>(*args)
 }
 
-@RestController
-@RequestMapping("/reactive")
-class ReactiveController {
-  @GetMapping("/hello")
-  fun getReactiveHello(): Mono<String> {
-    return Flux.just("Hello", "Reactive", "World")
-      .collect(Collectors.joining(" "))
-  }
-}
-
-@RestController
-@RequestMapping("/async")
-class AsyncController {
-  @GetMapping("/task")
-  fun getAsyncTask(): Mono<String> = Mono.just("Async Task Completed!")
-    .delayElement(Duration.ofSeconds(3))
-
-  @GetMapping("/tasks")
-  fun getAsyncTasks(): Flux<String> {
-    return Flux.just("Task 1", "Task 2", "Task 3")
-      .delayElements(Duration.ofSeconds(2))
+@Configuration
+class AsyncRoutesConfiguration {
+  @Bean
+  fun routeDefinition(): RouterFunction<ServerResponse> {
+    return route()
+      .path("/async") { router ->
+        router.GET("/task") { _ ->
+          return@GET Mono.just("Async Task Completed!")
+            .delayElement(Duration.ofSeconds(3))
+            .flatMap { task -> ServerResponse.ok().bodyValue(task) }
+        }
+        router.GET("/tasks") { _ ->
+          val taskFlux = Flux.just("Task 1", "Task 2", "Task 3")
+            .delaySequence(1.seconds.toJavaDuration())
+          return@GET ServerResponse.ok().body(taskFlux)
+        }
+      }
+      .build()
   }
 }
