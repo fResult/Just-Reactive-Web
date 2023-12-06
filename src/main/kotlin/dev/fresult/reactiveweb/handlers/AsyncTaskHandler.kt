971e85a35ef6a33832f0a3680cbdcbd9fded7664
fResult Sila <Styxmaz@gmail.com>
2023-12-07 00:35:38 +0700

    Refactor use coRouter

--- src/main/kotlin/dev/fresult/reactiveweb/handlers/AsyncTaskHandler.kt
+++ src/main/kotlin/dev/fresult/reactiveweb/handlers/AsyncTaskHandler.kt
@@ -1,25 +1,27 @@
 package dev.fresult.reactiveweb.handlers
 
+import kotlinx.coroutines.reactive.asFlow
+import kotlinx.coroutines.reactor.awaitSingle
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse
-import org.springframework.web.reactive.function.server.body
+import org.springframework.web.reactive.function.server.bodyAndAwait
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 import kotlin.time.Duration.Companion.seconds
 import kotlin.time.toJavaDuration
 
 @Component
-class AsyncTaskHandler() {
-  fun getTask(request: ServerRequest): Mono<ServerResponse> {
+class AsyncTaskHandler {
+  suspend fun getTask(request: ServerRequest): ServerResponse {
     return Mono.just("Async Task Completed!")
       .delayElement(3.seconds.toJavaDuration())
-      .flatMap(ServerResponse.ok()::bodyValue)
+      .flatMap(ServerResponse.ok()::bodyValue).awaitSingle()
   }
 
-  val getTasks = { _: ServerRequest ->
+  suspend fun getTasks(request: ServerRequest): ServerResponse {
     val taskFlux = Flux.just("Task 1", "Task 2", "Task 3")
       .delayElements(1.seconds.toJavaDuration())
-    ServerResponse.ok().body(taskFlux)
+    return ServerResponse.ok().bodyAndAwait(taskFlux.asFlow())
   }
 }
--- src/main/kotlin/dev/fresult/reactiveweb/routers/AsyncTaskRouter.kt
+++ src/main/kotlin/dev/fresult/reactiveweb/routers/AsyncTaskRouter.kt
@@ -4,18 +4,18 @@ import dev.fresult.reactiveweb.handlers.AsyncTaskHandler
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.reactive.function.server.RouterFunction
-import org.springframework.web.reactive.function.server.RouterFunctions.route
 import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.server.coRouter
 
 @Configuration
 class AsyncTaskRouter(private val handler: AsyncTaskHandler) {
   @Bean
   fun routeDefinition(): RouterFunction<ServerResponse> {
-    return route()
-      .path("/async") { router ->
-        router.GET("/task", handler::getTask)
-        router.GET("/tasks", handler.getTasks)
+    return coRouter {
+      "/async".nest {
+        GET("/task", handler::getTask)
+        GET("/tasks", handler::getTasks)
       }
-      .build()
+    }
   }
 }
