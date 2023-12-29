53637e3dab256c4b0120d001feb124c4c024eb10
fResult Sila <Styxmaz@gmail.com>
2023-12-29 13:31:19 +0700

    Prepare mocking database (local variable) in Repository and prepare Service and Handler

--- src/main/kotlin/dev/fresult/reactiveweb/handlers/TaskHandler.kt
+++ src/main/kotlin/dev/fresult/reactiveweb/handlers/TaskHandler.kt
@@ -1,22 +1,45 @@
 package dev.fresult.reactiveweb.handlers
 
+import dev.fresult.reactiveweb.TaskService
+import dev.fresult.reactiveweb.entities.TaskDTO
+import dev.fresult.reactiveweb.entities.TaskStatus
+import kotlinx.coroutines.reactive.asFlow
+import kotlinx.coroutines.reactor.awaitSingle
+import kotlinx.coroutines.runBlocking
 import org.springframework.stereotype.Component
-import org.springframework.web.reactive.function.server.ServerRequest
-import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.awaitBody
+import org.springframework.web.reactive.function.server.*
+import reactor.kotlin.core.publisher.toMono
 
 @Component
-class TaskHandler {
-  suspend fun all(request: ServerRequest): ServerResponse = TODO()
+class TaskHandler(val service: TaskService/*, val client: WebClient*/, builder: WebClient.Builder) {
+  private val client = builder.baseUrl("localhost").build()
+
+  suspend fun all(request: ServerRequest): ServerResponse {
+    TODO()
+  }
 
   suspend fun byId(request: ServerRequest): ServerResponse {
     val id: Long = request.pathVariable("id").toLong()
     TODO()
   }
 
-  suspend fun create(request: ServerRequest): ServerResponse = TODO()
+  suspend fun create(request: ServerRequest): ServerResponse {
+    val bodyRequest = request.bodyToMono<TaskDTO>()
+    TODO()
+  }
 
   suspend fun updateById(request: ServerRequest): ServerResponse {
     val id: Long = request.pathVariable("id").toLong()
+    val bodyRequest = request.bodyToMono<TaskDTO>()
+    TODO()
+  }
+
+  suspend fun updateStatusById(request: ServerRequest): ServerResponse {
+    val id: Long = request.pathVariable("id").toLong()
+    val action = request.pathVariable("status-action")
+    val updateStatusById = service.updateByAction(action)
     TODO()
   }
 
