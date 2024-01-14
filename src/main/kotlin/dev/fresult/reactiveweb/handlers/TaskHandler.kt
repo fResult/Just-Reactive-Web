bc7b1947d62e0b7b31ab6f9b1c0abb5fefe4ff63
fResult Sila <Styxmaz@gmail.com>
2024-01-14 23:56:28 +0700

    Refactor - DRY the duplicated code

--- src/main/kotlin/dev/fresult/reactiveweb/handlers/TaskHandler.kt
+++ src/main/kotlin/dev/fresult/reactiveweb/handlers/TaskHandler.kt
@@ -8,6 +8,7 @@ import kotlinx.coroutines.reactor.awaitSingle
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.server.*
+import reactor.core.publisher.Mono
 import reactor.kotlin.core.publisher.switchIfEmpty
 
 @Component
@@ -18,63 +19,53 @@ class TaskHandler(val service: TaskService) {
   }
 
   suspend fun byId(request: ServerRequest): ServerResponse {
-    val id: Long = request.pathVariable("id").toLong()
-    return service.byId(id).flatMap {
-      ServerResponse.ok().bodyValue(it.toDTO())
-    }.onErrorResume { exception ->
-      when (exception) {
-        is NoSuchElementException -> ServerResponse.status(HttpStatus.NOT_FOUND).bodyValue(exception.message.orEmpty())
-        else -> ServerResponse.status(HttpStatus.INTERNAL_SERVER_ERROR).bodyValue("Some thing went wrong")
-      }
-    }.awaitSingle()
+    val id = idFromParam(request)
+    return service.byId(id).flatMap(responseOkWithDTO)
+      .onErrorResume(::notFoundOrServerError).awaitSingle()
   }
 
   suspend fun create(request: ServerRequest): ServerResponse {
     return request.bodyToMono<TaskDTO>().flatMap { body ->
-      service.create(Task.fromDTO(body)).flatMap {
-        ServerResponse.status(HttpStatus.CREATED).bodyValue(it.toDTO())
-      }
-    }.switchIfEmpty {
-      ServerResponse.badRequest().bodyValue("Request Body cannot be null")
-    }.awaitSingle()
+      service.create(Task.fromDTO(body)).flatMap(responseCreatedWithDTO)
+    }.switchIfEmpty(::badRequestErrorWhenBodyIsEmpty).awaitSingle()
   }
 
   suspend fun updateById(request: ServerRequest): ServerResponse {
-    val id: Long = request.pathVariable("id").toLong()
+    val id = idFromParam(request)
     return request.bodyToMono<TaskDTO>().flatMap { body ->
-      service.updateById(id, Task.fromDTO(body)).flatMap { updatedTask ->
-        ServerResponse.ok().bodyValue(updatedTask.toDTO())
-      }.onErrorResume { exception ->
-        when (exception) {
-          is NoSuchElementException ->
-            ServerResponse.status(HttpStatus.NOT_FOUND).bodyValue(exception.message.orEmpty())
-
-          else -> ServerResponse.status(HttpStatus.INTERNAL_SERVER_ERROR).bodyValue("Some thing went wrong")
-        }
-      }
-    }.switchIfEmpty {
-      ServerResponse.badRequest().bodyValue("Request Body cannot be null")
-    }.awaitSingle()
+      service.updateById(id, Task.fromDTO(body)).flatMap(responseOkWithDTO)
+        .onErrorResume(::notFoundOrServerError)
+    }.switchIfEmpty(::badRequestErrorWhenBodyIsEmpty).awaitSingle()
   }
 
   suspend fun updateStatusById(request: ServerRequest): ServerResponse {
-    val id: Long = request.pathVariable("id").toLong()
+    val id = idFromParam(request)
     val action = request.pathVariable("status-action")
     val updateStatusById = service.updateByAction(action)
 
-    return updateStatusById(id).flatMap { updatedTask ->
-      ServerResponse.ok().bodyValue(updatedTask.toDTO())
-    }.onErrorResume { exception ->
-      when (exception) {
-        is NoSuchElementException -> ServerResponse.status(HttpStatus.NOT_FOUND).bodyValue(exception.message.orEmpty())
-        else -> ServerResponse.status(HttpStatus.INTERNAL_SERVER_ERROR).bodyValue("Some thing went wrong")
-      }
-    }.awaitSingle()
+    return updateStatusById(id).flatMap(responseOkWithDTO)
+      .onErrorResume(::notFoundOrServerError).awaitSingle()
   }
 
   suspend fun deleteById(request: ServerRequest): ServerResponse {
-    val id: Long = request.pathVariable("id").toLong()
+    val id = idFromParam(request)
     service.deleteById(id)
     return ServerResponse.noContent().buildAndAwait()
   }
+}
+
+private fun idFromParam(request: ServerRequest) = request.pathVariable("id").toLong()
+
+private val responseOkWithDTO = responseOneWithDTO(HttpStatus.OK)
+private val responseCreatedWithDTO = responseOneWithDTO(HttpStatus.CREATED)
+private fun responseOneWithDTO(status: HttpStatus): (Task) -> Mono<ServerResponse> = { task ->
+  ServerResponse.status(status).bodyValue(task.toDTO())
+}
+
+private fun badRequestErrorWhenBodyIsEmpty(): Mono<ServerResponse> =
+  ServerResponse.badRequest().bodyValue("Request Body cannot be null")
+
+private fun notFoundOrServerError(exception: Throwable): Mono<ServerResponse> = when (exception) {
+  is NoSuchElementException -> ServerResponse.status(HttpStatus.NOT_FOUND).bodyValue(exception.message.orEmpty())
+  else -> ServerResponse.status(HttpStatus.INTERNAL_SERVER_ERROR).bodyValue("Some thing went wrong")
 }
